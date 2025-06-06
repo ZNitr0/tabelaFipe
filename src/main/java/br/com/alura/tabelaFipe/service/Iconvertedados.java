301170b4b17ba9a2456fad247cc86f5ca084cc09
ZNitr0 <levidonascimento2410@gmail.com>
2025-06-05 23:33:32 -0300

    atualizando conversores de dados e passando a classe dados

--- src/main/java/br/com/alura/tabelaFipe/service/Iconvertedados.java
+++ src/main/java/br/com/alura/tabelaFipe/service/Iconvertedados.java
@@ -1,5 +1,9 @@
 package br.com.alura.tabelaFipe.service;
 
+import java.util.List;
+
 public interface Iconvertedados {
     <T> T obterDados(String Json, Class<T> classe);
+
+    <T> List<T>obterLista(String Json, Class<T> classe);
 }
--- src/main/java/br/com/alura/tabelaFipe/service/converteDados.java
+++ src/main/java/br/com/alura/tabelaFipe/service/converteDados.java
@@ -2,6 +2,9 @@ package br.com.alura.tabelaFipe.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 public class converteDados implements Iconvertedados {
 
@@ -11,6 +14,17 @@ public class converteDados implements Iconvertedados {
     public <T> T obterDados(String json, Class<T> classe) {
         try {
             return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//     construção de lista
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) {
+        CollectionType lista = mapper.getTypeFactory().constructCollectionType(List.class, classe);
+
+        try {
+            return mapper.readValue(json, lista);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
